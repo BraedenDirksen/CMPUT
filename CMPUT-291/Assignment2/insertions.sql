b8c2ed98c6a6f15b89623a297fcb1af4293afaa8
braeden dirksen <braedendirksen@gmail.com>
2020-10-06 18:20:04 -0600

    started query four and added the insertions i needed to test the querry

--- CMPUT-291/Assignment2/insertions.sql
+++ CMPUT-291/Assignment2/insertions.sql
@@ -6,15 +6,17 @@ VALUES (1,'braeden','edmonton',DATE('2020-10-04')),
 
 INSERT INTO posts
 VALUES (1,DATE('2020-10-04'),'question post','this is a question post',1),
-(2,DATE('2020-10-05'),'answer post','this is a answer post',2),
-(3,DATE('2020-10-04'),'relational database','this is a answer post',3)
+(2,DATE('2020-10-04'),'answer post','this is a answer post',1),
+(3,DATE('2020-10-04'),'relational database','this is a answer post',2),
+(4,DATE('2020-10-04'),'answer post','this is a answer post 2',1),
+(5,DATE('2020-10-04'),'question post','this is a question post',2)
 ;
 
 INSERT INTO questions
-VALUES (1,2);
+VALUES (1,2),(2,5);
 
 INSERT INTO answers
-VALUES (2,1);
+VALUES (2,1),(4,1),(3,5);
 
 INSERT INTO badges
 VALUES ('gold','gold'),
--- CMPUT-291/Assignment2/queries.sql
+++ CMPUT-291/Assignment2/queries.sql
@@ -1,4 +1,4 @@
-/* querry one COMPLETE*/
+/* query one COMPLETE*/
 SELECT uid
 FROM ubadges
 WHERE bname = 'gold'
@@ -9,7 +9,7 @@ SELECT poster
 FROM posts
 WHERE pid IN (SELECT pid FROM questions);
 
-/*querry two */
+/*query two */
 SELECT pid, title
 FROM posts
 WHERE title LIKE '%relational database%'
@@ -20,24 +20,29 @@ AND pid IN (SELECT pid
 FROM tags
 WHERE tag LIKE "%atabase%"));
 
-/*querry three COMPLETE*/
+/*query three COMPLETE*/
 SELECT p1.pid, p1.pdate
 FROM posts p1, posts p2, questions, answers
 WHERE p1.pid = questions.pid
 AND p2.pid = answers.pid
 AND answers.qid = p1.pid
-AND 3 >= (julianday(p2.pdate) - julianday(p1.pdate))
+AND 3 >= (julianday(p2.pdate) - julianday(p1.pdate));
 
-/*querry four */
+/*query four need to figure out how to check for two answers*/
+SELECT p1.poster
+FROM posts p1, posts p2, questions q, answers a
+WHERE p1.pid = q.pid
+AND p2.pid = a.pid
+AND p2.poster = p1.poster
 
-/*querry five */
+/*query five */
 
-/*querry six */
+/*query six */
 
-/*querry seven */
+/*query seven */
 
-/*querry eight */
+/*query eight */
 
-/*querry nine */
+/*query nine */
 
-/*querry ten */
+/*query ten */
