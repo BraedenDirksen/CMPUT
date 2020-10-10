0b3062321bb41d5ba713f44e6f00ebebd089a512
unjustodin <braedendirksen@gmail.com>
2020-10-10 17:23:41 -0600

    added offictial database tables and inserts, also finished q9

--- CMPUT-291/Assignment2/official/queries.sql
+++ CMPUT-291/Assignment2/official/queries.sql
@@ -109,8 +109,30 @@ SELECT DISTINCT posts.poster as user_id, question_amount, answer_amount, vote_ou
                 FROM posts, votes
                 where posts.pid = votes.pid
                 group by posts.poster) as v_in
-    on posts.poster = v_in.poster
+    on posts.poster = v_in.poster;
 /*query nine */
+DROP VIEW questioninfo;
 
+CREATE VIEW questioninfo(pid,uid,theaid,voteCnt,ansCnt) AS
+    SELECT DISTINCT questions.pid,poster,qinfo.theaid, voteCnt, ansCnt
+        FROM questions
+        LEFT JOIN
+            (SELECT questions.pid,poster,theaid
+            FROM posts, questions, answers, votes
+            WHERE posts.pid = questions.pid
+            GROUP BY questions.pid) AS qinfo
+            ON qinfo.pid = questions.pid
+        LEFT JOIN
+            (SELECT questions.pid,count(*) AS ansCnt
+            FROM answers, questions
+            WHERE answers.qid = questions.pid
+            GROUP BY questions.pid) AS ans
+            ON ans.pid = questions.pid
+        LEFT JOIN
+            (SELECT questions.pid,count(*) AS voteCnt
+            FROM votes, questions
+            WHERE votes.pid = questions.pid
+            GROUP BY questions.pid) AS vote
+            ON vote.pid = questions.pid
 
 /*query ten */
